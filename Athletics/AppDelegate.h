55e771cc95f0f3a4f44823e32f942d918f4d51e1
c1173302633 <c1173302633@126.com>
2016-04-09 18:10:33 +0800

    上传项目
    fff

---
+++ Athletics/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Athletics
+//
+//  Created by 李宛 on 16/4/9.
+//  Copyright © 2016年 李宛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
