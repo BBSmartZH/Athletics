1c57b35fb9d13f93a99ea682e80cde73e254a19e
c278518231 <c278518231@126.com>
2016-04-11 11:28:53 +0800

    re

---
+++ Athletics/UI/Events/View/Home/LWNewsTypeNormalCell.h
@@ -0,0 +1,13 @@
+//
+//  LWNewsTypeNormalCell.h
+//  Athletics
+//
+//  Created by 李宛 on 16/4/11.
+//  Copyright © 2016年 李宛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LWNewsTypeNormalCell : UITableViewCell
+
+@end
---
+++ Athletics/UI/Events/View/Home/LWNewsTypeNormalCell.m
@@ -0,0 +1,23 @@
+//
+//  LWNewsTypeNormalCell.m
+//  Athletics
+//
+//  Created by 李宛 on 16/4/11.
+//  Copyright © 2016年 李宛. All rights reserved.
+//
+
+#import "LWNewsTypeNormalCell.h"
+
+@implementation LWNewsTypeNormalCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
